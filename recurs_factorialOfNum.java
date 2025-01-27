556fc96546d206a905cbcb3e8099d092e00a47c0
shravanigarje <shravanigarje15@gmail.com>
2025-01-27 20:23:50 +0530

    Add files via upload

---
+++ recurs_factorialOfNum.java
@@ -0,0 +1,18 @@
+public class recurs_factorialOfNum {
+ 
+    public static int calfactorial(int n){
+        if(n == 1 || n == 0){
+            return 1;
+        }
+       
+        int fact_nm1= calfactorial(n-1);
+        int fact_n = n* fact_nm1;
+        return fact_n;
+    
+     }
+    public static void main(String[] args) {
+        int n = 5;
+        int ans = calfactorial(n);
+        System.out.println(ans);
+    }
+}
